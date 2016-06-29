2f138f3d91331b89aa30df162bda7886f6805b52
ands <amanti@gmx.de>
2016-06-29 22:45:17 +0200

    optional simpler scaling filter, separated scaling and sending to measure performance.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	$(CC) -std=c99 -O3 -o client *.c -lX11 -lXrandr
+	$(CC) -std=c99 -O3 -o client *.c -lX11 -lXrandr -lm
--- client.c
+++ client.c
@@ -1,14 +1,15 @@
-#define _DEFAULT_SOURCE 1
+//#define _DEFAULT_SOURCE 1
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
@@ -38,7 +39,9 @@ int main(int argc, char *argv[])
 	SizeID original_size_id = XRRConfigCurrentConfiguration(conf, &original_rotation);
 	int in_w = xrrs[original_size_id].width;
 	int in_h = xrrs[original_size_id].height;
-	
+	uint8_t *out_data = malloc(out_w * out_h * 3);
+	float dx = in_w / (float)out_w, dy = in_h / (float)out_h;
+
 	// network stuff
 	struct hostent *server;
 	server = gethostbyname(host);
@@ -68,55 +71,70 @@ int main(int argc, char *argv[])
 	while (42)
 	{
 		XImage *pic = XGetImage(display, window, 0, 0, in_w, in_h, AllPlanes, ZPixmap);
+
+		//clock_t start_scale = clock();
 		int bpp = pic->bits_per_pixel / 8;
+
+		uint8_t *out = out_data;
+		float fx = 0.0f, fy = 0.0f;
+		int ix = 0, iy = 0;
+		float dx = in_w / (float)out_w, dy = in_h / (float)out_h;
+	
+		float ftl = 1.0f, ftr = 0.0f, fbl = 0.0f, fbr = 0.0f;
+	
+		for (int y = 0; y < out_h; y++)
+		{
+			float nfy = 1.0f - fy;
+
+			uint8_t *tl = (unsigned char*)pic->data + iy * pic->bytes_per_line;
+			uint8_t *tr = tl + bpp;
+			uint8_t *bl = tl + pic->bytes_per_line;
+			uint8_t *br = tr + pic->bytes_per_line;
+			fx = 0.0f;
+			ix = 0;
+			for (int x = 0; x < out_w; x++)
+			{
+			#if 1 // bilinear interpolated scaling
+				float nfx = 1.0f - fx;
+
+				float ftl = nfx * nfy;
+				float ftr =  fx * nfy;
+				float fbl = nfx *  fy;
+				float fbr =  fx *  fy;
+
+				out[2] = (uint8_t)(tl[0] * ftl + tr[0] * ftr + bl[0] * fbl + br[0] * fbr);
+				out[1] = (uint8_t)(tl[1] * ftl + tr[1] * ftr + bl[1] * fbl + br[1] * fbr);
+				out[0] = (uint8_t)(tl[2] * ftl + tr[2] * ftr + bl[2] * fbl + br[2] * fbr);
+			#else // averaged scaling (faster, good enough?)
+				out[2] = (tl[0] + tr[0] + bl[0] + br[0]) >> 2;
+				out[1] = (tl[1] + tr[1] + bl[1] + br[1]) >> 2;
+				out[0] = (tl[2] + tr[2] + bl[2] + br[2]) >> 2;
+			#endif
+				out += 3;
+				fx += dx;
+				int inc_x = (int)fx, step = inc_x * bpp;
+				fx -= inc_x;
+				ix += inc_x;
+				tl += step; tr += step;
+				bl += step; br += step;
+			}
+			fy += dy;
+			int inc_y = (int)fy;
+			fy -= inc_y;
+			iy += inc_y;
+		}
+		pic->f.destroy_image(pic);
+		//printf("Scale: %3dms\n", (clock() - start_scale) * 1000 / CLOCKS_PER_SEC);
+
+		//clock_t start_send = clock();
 		for (int y = 0; y < out_h; y += lines_per_packet)
 		{
 			header[1] = y;
-			for (int ly = 0; ly < lines_per_packet && y + ly < out_h; ly++)
-			{
-				uint8_t *in = (unsigned char*)pic->data + ((y + ly) * in_h / out_h) * pic->bytes_per_line;
-				uint8_t *out = buffer + 6 + 3 * ly * out_w;
-				
-				// append a line of bilinear interpolated pixel values to output buffer
-				float fy = (y + ly) * in_h / (float)out_h;
-				int iy = (int)fy;
-				fy -= iy;
-				float nfy = nfy = 1.0f - fy;
-				for (int x = 0; x < out_w; x++)
-				{
-					float fx = x * in_w / (float)out_w;
-					int ix = (int)fx;
-					fx -= ix;
-					float nfx = 1.0f - fx;
-					
-					uint8_t *tl = in + ix * bpp;
-					uint8_t *tr = tl + bpp;
-					uint8_t *bl = tl + pic->bytes_per_line;
-					uint8_t *br = tr + pic->bytes_per_line;
-					
-					float t[3] = {
-						tl[2] * nfx + tr[2] * fx,
-						tl[1] * nfx + tr[1] * fx,
-						tl[0] * nfx + tr[0] * fx };
-					float b[3] = {
-						bl[2] * nfx + br[2] * fx,
-						bl[1] * nfx + br[1] * fx,
-						bl[0] * nfx + br[0] * fx };
-					float result[3] = {
-						t[0] * nfy + b[0] * fy,
-						t[1] * nfy + b[1] * fy,
-						t[2] * nfy + b[2] * fy };
-					
-					out[0] = (uint8_t)result[0];
-					out[1] = (uint8_t)result[1];
-					out[2] = (uint8_t)result[2];
-					out += 3;
-				}
-			}
+			memcpy(buffer + 6, out_data + 3 * y * out_w, 3 * lines_per_packet * out_w);
 			sendto(sockfd, buffer, 6 + lines_per_packet * out_w * 3, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-			//usleep(15000 / (out_h / lines_per_packet)); // only send fast enough for ~60fps
 		}
-		pic->f.destroy_image(pic);
+		//printf("Send:  %3dms\n\n", (clock() - start_send) * 1000 / CLOCKS_PER_SEC);
 	}
+	free(out_data);
 	return 0;
 }
